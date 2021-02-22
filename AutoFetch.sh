a5c5d1babd0dcc6f65ba6be19d64b07a7e710173
Daksh <dak-x@outlook.com>
2021-02-22 08:44:37 +0530

    Last updated : Mon 22 Feb 08:44:37 IST 2021

--- AutoFetch.sh
+++ AutoFetch.sh
@@ -3,6 +3,8 @@ cp -r ~/.config/alacritty ./
 cp -r ~/.config/fish ./
 cp ~/.config/Code/User/settings.json ./vsCode/settings.json
 cp ~/.config/Code/User/keybindings.json ./vsCode/keybindings.json
+cp -r ~/.config/polybar/blocks ./polybar/
+cp ~/.xmonad/xmonad.hs ./
 echo "Last updated :" (date) > msg.txt
 git add .
 git commit -F msg.txt
--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Will update my gradually increasing configuration files.
 1. alacritty
 2. vsCode
 3. fish
+4. xMonad
+5. polybar [Ack: https://github.com/ngynLk/polybar-themes]
